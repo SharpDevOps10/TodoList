11687a53e29dddf00c78bb1cd6ec72dc0b2fbf65
dtimofeev <td090833@gmail.com>
2023-03-15 17:51:33 +0200

    Added new type `TaskType`

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+type TaskType = {
+  id : number,
+  title : string,
+  isDone : boolean,
+};
 type PropsType = {
   title : string,
+  tasks : Array<TaskType>,
 };
 export function Todolist(props : PropsType) {
 
