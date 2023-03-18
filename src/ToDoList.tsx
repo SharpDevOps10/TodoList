8f236fa0d17b0005482cab985a6c5ae081279f7d
dtimofeev <td090833@gmail.com>
2023-03-18 13:34:55 +0200

    Ref : moved {all,active,completed} states from the component to `onXClickHandler`

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -30,6 +30,10 @@ export function Todolist(props: PropsType) {
     props.addTask(newTaskTitle);
     setNewTaskTitle("");
   };
+  const onAllClickHandler = () => props.changeFilter("all");
+  const onActiveClickHandler = () => props.changeFilter("active");
+  const onCompletedClickHandler = () => props.changeFilter("completed");
+
 
   return <div>
     <h3>{props.title}</h3>
@@ -54,9 +58,9 @@ export function Todolist(props: PropsType) {
       }
     </ul>
     <div>
-      <button onClick={() => props.changeFilter("all")}>All</button>
-      <button onClick={() => props.changeFilter("active")}>Active</button>
-      <button onClick={() => props.changeFilter("completed")}>Completed</button>
+      <button onClick={onAllClickHandler}>All</button>
+      <button onClick={onActiveClickHandler}>Active</button>
+      <button onClick={onCompletedClickHandler}>Completed</button>
     </div>
   </div>
 
