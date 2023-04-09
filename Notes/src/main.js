c279388a200a41bcd2e8ed435e65476c6d293246
SharpDevOps10 <td090833@gmail.com>
2023-04-09 20:04:24 +0300

    Improved editButton listener

--- Notes/src/main.js
+++ Notes/src/main.js
@@ -5,18 +5,22 @@ const createNote = (title, text) => {
   noteEl.classList.add('note');
   noteEl.innerHTML = `
     <div class="note-header">
-      <p>${title}</p>
+      <p id="note-title">${title}</p>
+      <textarea id="note-title-input" class="hidden">${title}</textarea>
       <div class="note-actions"> 
         <button class="note-edit"><i class="fa-solid fa-pen-to-square"></i></button>
         <button class="note-delete"><i class="fa-solid fa-trash"></i></button>
       </div>
     </div>
-    <p>${text}</p>
+    <p id="note-text">${text}</p>
   `;
   const editBtn = noteEl.querySelector('.note-edit');
   const deleteBtn = noteEl.querySelector('.note-delete');
+  const titleEl = noteEl.querySelector('#note-title');
+  const textEl = noteEl.querySelector('#note-text');
   editBtn.addEventListener('click', (e) => {
-
+    titleEl.classList.toggle('hidden');
+    textEl.classList.toggle('hidden');
   });
   deleteBtn.addEventListener('click', (e) => {
     noteEl.remove();
