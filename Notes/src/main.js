9976b21667c3a18df9c449b88d8e04014bad4ed2
SharpDevOps10 <td090833@gmail.com>
2023-04-09 20:13:39 +0300

    Ref: improved code formatting using prettier

--- Notes/src/main.js
+++ Notes/src/main.js
@@ -1,8 +1,8 @@
-const notesEl = document.querySelector('.notes');
-const addBtn = document.querySelector('.note-add');
+const notesEl = document.querySelector(".notes");
+const addBtn = document.querySelector(".note-add");
 const createNote = (title, text) => {
-  const noteEl = document.createElement('div');
-  noteEl.classList.add('note');
+  const noteEl = document.createElement("div");
+  noteEl.classList.add("note");
   noteEl.innerHTML = `
     <div class="note-header">
       <p id="note-title">${title}</p>
@@ -15,35 +15,35 @@ const createNote = (title, text) => {
     <p id="note-text">${text}</p>
     <textarea id="note-textarea" class="hidden">${text}</textarea>
   `;
-  const editBtn = noteEl.querySelector('.note-edit');
-  const deleteBtn = noteEl.querySelector('.note-delete');
-  const titleEl = noteEl.querySelector('#note-title');
-  const textEl = noteEl.querySelector('#note-text');
-  const titleInputEl = noteEl.querySelector('#note-title-input');
-  const textInputEl = noteEl.querySelector('#note-textarea');
+  const editBtn = noteEl.querySelector(".note-edit");
+  const deleteBtn = noteEl.querySelector(".note-delete");
+  const titleEl = noteEl.querySelector("#note-title");
+  const textEl = noteEl.querySelector("#note-text");
+  const titleInputEl = noteEl.querySelector("#note-title-input");
+  const textInputEl = noteEl.querySelector("#note-textarea");
 
-  editBtn.addEventListener('click', () => {
-    titleEl.classList.toggle('hidden');
-    textEl.classList.toggle('hidden');
+  editBtn.addEventListener("click", () => {
+    titleEl.classList.toggle("hidden");
+    textEl.classList.toggle("hidden");
 
-    titleInputEl.classList.toggle('hidden');
-    textInputEl.classList.toggle('hidden');
+    titleInputEl.classList.toggle("hidden");
+    textInputEl.classList.toggle("hidden");
   });
 
-  deleteBtn.addEventListener('click', () => {
+  deleteBtn.addEventListener("click", () => {
     noteEl.remove();
   });
-  titleInputEl.addEventListener('input', (e) => {
+  titleInputEl.addEventListener("input", (e) => {
     titleEl.innerText = e.target.value;
   });
 
-  textInputEl.addEventListener('input', (e) => {
+  textInputEl.addEventListener("input", (e) => {
     textEl.innerText = e.target.value;
   });
 
   return noteEl;
 };
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener("click", () => {
   const element = createNote("Header", "Your text");
   notesEl.appendChild(element);
 });
