aa127e395366cde427b835f28c7c6db33c8795c9
SharpDevOps10 <td090833@gmail.com>
2023-04-15 23:40:46 +0300

    Added `removeTodoListHandler`

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -14,7 +14,8 @@ type PropsType = {
   changeFilter: (value: FilterValuesTypes, todoListId: string) => void,
   addTask: (title: string, todoListId: string) => void,
   changeTaskStatus: (taskID: string, isDone: boolean, todoListId: string) => void,
-  filter : FilterValuesTypes,
+  filter: FilterValuesTypes,
+  removeTodoList: (todoListId: string) => void,
 };
 
 export function Todolist(props: PropsType) {
@@ -43,10 +44,13 @@ export function Todolist(props: PropsType) {
   const onAllClickHandler = () => props.changeFilter("all", props.id);
   const onActiveClickHandler = () => props.changeFilter("active", props.id);
   const onCompletedClickHandler = () => props.changeFilter("completed", props.id);
+  const removeTodoListHandler = () => {
+    props.removeTodoList(props.id);
+  };
 
 
   return <div>
-    <h3>{props.title}</h3>
+    <h3>{props.title} <button onClick={removeTodoListHandler}>x</button></h3>
     <div>
       <input value={newTaskTitle}
              onChange={onChangeHandler}
