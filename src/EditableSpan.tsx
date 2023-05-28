4f0d583c358a1a1084fd903b6ef03d93ff23ef57
SharpDevOps10 <td090833@gmail.com>
2023-05-28 16:14:19 +0300

    Add capability to input your data

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 
 type EditableSpanType = {
   title: string,
@@ -6,11 +6,21 @@ type EditableSpanType = {
 
 export function EditableSpan(props: EditableSpanType) {
   const [editMode, setEditMode] = useState(false);
-  const activateEditMode = () => setEditMode(true);
-  const activateViewMode = () => setEditMode(false);
+  const [title, setTitle] = useState(" ");
+
+  const activateEditMode = () => {
+    setEditMode(true);
+    setTitle(props.title);
+  };
+  const activateViewMode = () => {
+    setEditMode(false);
+  };
+  const onChangeTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value);
+  }
 
 
   return editMode
-    ? <input value={props.title} onBlur={activateViewMode}/>
+    ? <input value={props.title} onBlur={activateViewMode} onChange={onChangeTitleHandler}/>
     : <span onDoubleClick={activateEditMode}>{props.title}</span>;
 }
