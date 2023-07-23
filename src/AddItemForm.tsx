78176750b4c9b5a1966b7c466fa6456840e85a01
SharpDevOps10 <td090833@gmail.com>
2023-07-23 15:35:23 +0300

    Change the button style

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import {Button} from "@material-ui/core";
 
 type AddItemFormPropsType = {
   addItem: (title: string) => void,
@@ -35,7 +36,7 @@ export function AddItemForm(props: AddItemFormPropsType) {
            onKeyPress={onKeyPressHandler}
            className={error ? "error" : ""}
     />
-    <button onClick={addTask}>+</button>
+    <Button onClick={addTask} variant={'contained'} color={'primary'}>+</Button>
     {error && <div className="error-message">{error}</div>}
   </div>
 }
