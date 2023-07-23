a8168acff0641639903be8ec5d6e44fabc1be3f9
SharpDevOps10 <td090833@gmail.com>
2023-07-23 16:26:51 +0300

    Update input

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,5 +1,5 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from "react";
-import {Button} from "@material-ui/core";
+import {Button, TextField} from "@material-ui/core";
 
 type AddItemFormPropsType = {
   addItem: (title: string) => void,
@@ -31,12 +31,14 @@ export function AddItemForm(props: AddItemFormPropsType) {
     }
   };
   return <div>
-    <input value={newTaskTitle}
-           onChange={onChangeHandler}
-           onKeyPress={onKeyPressHandler}
-           className={error ? "error" : ""}
+    <TextField value={newTaskTitle}
+               variant={'outlined'}
+               label={"Type value"}
+               onChange={onChangeHandler}
+               onKeyPress={onKeyPressHandler}
+               error={!!error}
+               helperText={error}
     />
     <Button onClick={addTask} variant={'contained'} color={'primary'}>+</Button>
-    {error && <div className="error-message">{error}</div>}
   </div>
 }
