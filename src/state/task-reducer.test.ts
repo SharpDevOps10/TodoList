75885fd2f8c0a7930cc178df64face9f7d46f575
SharpDevOps10 <td090833@gmail.com>
2023-11-04 20:34:06 +0200

    feature #31: implemented adding task
    closes #31

--- src/state/task-reducer.test.ts
+++ src/state/task-reducer.test.ts
@@ -4,13 +4,13 @@ import {addTaskAC, removeTaskAC, taskReducer} from './task-reducer';
 test('correct task should be deleted from correct array', () => {
   const startState: TasksState = {
     'todolist1Id': [
-      { id: '1', title: "CSS", isDone: true },
-      { id: '2', title: "JS", isDone: true },
-      { id: '3', title: "React", isDone: false },
+      { id: '1', title: 'CSS', isDone: true },
+      { id: '2', title: 'JS', isDone: true },
+      { id: '3', title: 'React', isDone: false },
     ],
     'todolist2Id': [
-      { id: '1', title: "The Sorrows Of Werther", isDone: false },
-      { id: '2', title: "Milk", isDone: true },
+      { id: '1', title: 'The Sorrows Of Werther', isDone: false },
+      { id: '2', title: 'Milk', isDone: true },
     ],
   };
 
@@ -25,17 +25,17 @@ test('correct task should be deleted from correct array', () => {
 test('correct task should be added to correct array', () => {
   const startState: TasksState = {
     'todolist1Id': [
-      { id: '1', title: "CSS", isDone: true },
-      { id: '2', title: "JS", isDone: true },
-      { id: '3', title: "React", isDone: false },
+      { id: '1', title: 'CSS', isDone: true },
+      { id: '2', title: 'JS', isDone: true },
+      { id: '3', title: 'React', isDone: false },
     ],
     'todolist2Id': [
-      { id: '1', title: "The Sorrows Of Werther", isDone: false },
-      { id: '2', title: "Milk", isDone: true },
+      { id: '1', title: 'The Sorrows Of Werther', isDone: false },
+      { id: '2', title: 'Milk', isDone: true },
     ],
   };
 
-  const action = addTaskAC('juice');
+  const action = addTaskAC('juice', 'todolist2Id');
   const endState = taskReducer(startState, action);
 
   expect(endState['todolist1Id'].length).toBe(3);
